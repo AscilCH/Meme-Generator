b1d3459827d49823c7ed15727264be49456b17c5
acsciltheboss <assil.chtioui.css15@gmail.com>
2024-08-13 00:24:03 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from'./components/Header'
+import Meme from './components/Meme'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+    <Header />
+    <Meme />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="https://th.bing.com/th/id/R.d824a606cd3cf1114def94a70e5d5d5a?rik=GjAm%2fEccZ7usXQ&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fyco%2faek%2fycoaekzki.png&ehk=FlXeG65zpWhIVumsJxSGuO4yui2Ex2mtwTD5tEzVtXg%3d&risl=&pid=ImgRaw&r=0" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">InfectV2-2G Project 1</h4>
+        </header>
+    )
+}
